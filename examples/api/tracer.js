f341c1f29a23d9d6e0bb79aeddb448a42002c160
Mykal Machon <mykal@tinybox.dev>
2025-03-16 13:28:52 -0700

    feat: add complete example for tempo traces
    This inclues a proper example of how to use tempo traces in an express app via the OTel API packages.

---
+++ examples/api/tracer.js
@@ -0,0 +1,33 @@
+import { NodeSDK } from "@opentelemetry/sdk-node";
+import { getNodeAutoInstrumentations } from "@opentelemetry/auto-instrumentations-node";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { Resource } from "@opentelemetry/resources";
+
+const tempoUrl = process.env.TEMPO_URL || "http://localhost:4318";
+const tempoIngestAPIUrl = `${tempoUrl}/v1/traces`;
+
+// Configure the trace exporter
+const traceExporter = new OTLPTraceExporter({
+  url: tempoIngestAPIUrl
+});
+
+// Create and register the SDK
+const sdk = new NodeSDK({
+  resource: new Resource({
+    "service.name": process.env.TEMPO_SERVICE_NAME || 'unknown',
+  }),
+  traceExporter: traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+});
+
+// Initialize the SDK and register with the OpenTelemetry API
+sdk.start();
+
+// Gracefully shut down the SDK on process exit
+process.on("SIGTERM", () => {
+  sdk
+    .shutdown()
+    .then(() => console.log("Tracing terminated"))
+    .catch((error) => console.log("Error terminating tracing", error))
+    .finally(() => process.exit(0));
+});
